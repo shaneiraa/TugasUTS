c0808adc6336a4814110bc1eb71216c2cbfccc90
ASUS <serinaisah3@gmail.com>
2025-11-07 21:12:59 +0700

    TUGAS UTS

--- app/src/main/java/com/example/tugasuts/MainActivity.kt
+++ app/src/main/java/com/example/tugasuts/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,45 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val editUsername = findViewById<EditText>(R.id.editUsername)
+        val editEmail = findViewById<EditText>(R.id.editEmail)
+        val editFirstName = findViewById<EditText>(R.id.editFirstName)
+        val editLastName = findViewById<EditText>(R.id.editLastName)
+        val editPassword = findViewById<EditText>(R.id.editPassword)
+        val btnSubmit = findViewById<Button>(R.id.btnSubmit)
+        val btnCancel = findViewById<Button>(R.id.btnCancel)
+
+        btnSubmit.setOnClickListener {
+            // Validasi semua field tidak boleh kosong
+            if (isAllFieldsFilled(editUsername, editEmail, editFirstName, editLastName, editPassword)) {
+                // Semua field terisi
+                val username = editUsername.text.toString()
+                val email = editEmail.text.toString()
+                val firstName = editFirstName.text.toString()
+                val lastName = editLastName.text.toString()
+
+                // Tampilkan pesan sukses
+                val successMessage = "User $firstName $lastName (Username: $username) berhasil didaftarkan"
+                Toast.makeText(this, successMessage, Toast.LENGTH_LONG).show()
+            } else i
+        }
+
+        }
     }
-}
+
+
+    private fun isAllFieldsFilled(
+        editUsername: EditText,
+        editEmail: EditText,
+        editFirstName: EditText,
+        editLastName: EditText,
+        editPassword: EditText
+    ): Boolean {
+        return editUsername.text.toString().isNotEmpty() &&
+                editEmail.text.toString().isNotEmpty() &&
+                editFirstName.text.toString().isNotEmpty() &&
+                editLastName.text.toString().isNotEmpty() &&
+                editPassword.text.toString().isNotEmpty()
+    }
